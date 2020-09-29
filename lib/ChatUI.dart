76b3dc076d0fc19753405eb65eea613844603207
CWTBT <lw03232@gmail.com>
2020-09-29 15:05:55 -0500

    added very basic widget layout

--- lib/ChatUI.dart
+++ lib/ChatUI.dart
@@ -11,9 +11,37 @@ class _ChatroomState extends State<Chatroom> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return Scaffold (
+      appBar: AppBar(
+        title: Text("Mafia Chatroom"),
+      ),
       body: Center (
-        child: Text("Chatroom"),
+        child: Column(
+          children: [
+            Container (
+              decoration: BoxDecoration (
+                color: Colors.grey,
+                border: Border.all (
+                  color: Colors.black,
+                ),
+              ),
+              height: 500.0,
+              width: 400.0,
+              margin: const EdgeInsets.all(10.0),
+            ),
+            Container (
+              decoration: BoxDecoration (
+                color: Colors.grey,
+                border: Border.all (
+                  color: Colors.black,
+                ),
+              ),
+              child: TextField(),
+              margin: EdgeInsets.all(10.0),
+              padding: EdgeInsets.fromLTRB(10.0, 0, 0, 0),
+            ),
+          ],
+        ),
       ),
     );
   }
