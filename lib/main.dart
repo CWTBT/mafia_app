85b292f82d48e917ec900e01c0058c100b0f5b1f
thoopchuk <thoopchuk26@users.noreply.github.com>
2020-09-30 13:53:06 -0500

    Copied Ferrer's test_messenger

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,20 @@
+import 'dart:async';
+import 'dart:io';
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
+import 'package:text_messenger/data.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
+      title: 'Network Demo',
+      theme: ThemeData(primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
@@ -34,15 +25,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -50,68 +32,156 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  Friends _friends;
+  String _currentFriend;
+  List<DropdownMenuItem<String>> _friendList;
+  TextEditingController _nameController, _ipController, _sendController;
+  Widget Function(BuildContext) _screenFunction;
 
-  void _incrementCounter() {
+  void initState() {
+    super.initState();
+    _screenFunction = _mainScreen;
+    _friends = Friends();
+    _friends.add("Self", "127.0.0.1");
+    _currentFriend = "Self";
+    print("currentFriend: $_currentFriend");
+    _nameController = TextEditingController(text: _currentFriend);
+    _ipController = TextEditingController(text: _friends.ipAddr(_currentFriend));
+    _sendController = TextEditingController();
+    _setupServer();
+  }
+
+  Future<void> _setupServer() async {
+    try {
+      ServerSocket server =
+      await ServerSocket.bind(InternetAddress.anyIPv4, ourPort);
+      server.listen(_listenToSocket); // StreamSubscription<Socket>
+    } on SocketException catch (e) {
+      _sendController.text = e.message;
+    }
+  }
+
+  void _listenToSocket(Socket socket) {
+    socket.listen((data) {
+      setState(() {
+        _handleIncomingMessage(socket.remoteAddress.address, data);
+      });
+    });
+  }
+
+  void _handleIncomingMessage(String ip, Uint8List incomingData) {
+    String received = String.fromCharCodes(incomingData);
+    print("Received '$received' from '$ip'");
+    _friends.receiveFrom(ip, received);
+    _currentFriend = _friends.getName(ip);
+  }
+
+  // From https://medium.com/@boldijar.paul/comboboxes-in-flutter-cabc9178cc95
+  List<DropdownMenuItem<String>> makeFriendList() {
+    print("making friend list");
+    List<DropdownMenuItem<String>> items = new List();
+    for (String friend in _friends) {
+      items.add(DropdownMenuItem(value: friend, child: Text(friend)));
+    }
+    print("${items.length} friends");
+    return items;
+  }
+
+  void updateFriendList(String selectedFriend) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _currentFriend = selectedFriend;
+    });
+  }
+
+  void addFriend() {
+    setState(() {
+      _nameController.text = "";
+      _ipController.text = "";
+      _screenFunction = _newFriendScreen;
+    });
+  }
+
+  void addNew() {
+    setState(() {
+      _friends.add(_nameController.text, _ipController.text);
+      _currentFriend = _nameController.text;
+      _screenFunction = _mainScreen;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
+        child: _screenFunction(context),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  Widget _mainScreen(BuildContext context) {
+    _friendList = makeFriendList();
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        DropdownButton(
+          value: _currentFriend,
+          items: _friendList,
+          onChanged: updateFriendList,
+        ),
+        RaisedButton(child: Text("Add Friend"), onPressed: addFriend,),
+        historyBox(),
+        makeActionText(200, "Send to $_currentFriend", _sendController, send),
+      ],
+    );
+  }
+
+  Widget _newFriendScreen(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget> [
+        makeTextEntry(200, "Name", _nameController),
+        makeTextEntry(200, "IP Address", _ipController),
+        RaisedButton(child: Text("Add"), onPressed: addNew),
+      ],
+    );
+  }
+
+  Widget makeTextEntry(double width, String label, TextEditingController controller) {
+    return makeActionText(width, label, controller, (s) {});
+  }
+
+  Widget makeActionText(double width, String label, TextEditingController controller, void Function(String) handler) {
+    return SizedBox(width: width,
+        child: TextField(controller: controller, onSubmitted: handler,
+          decoration: InputDecoration(labelText: label),));
+  }
+
+  Widget historyBox() {
+    // Concept from:  https://stackoverflow.com/questions/49638499/how-to-make-the-scrollable-text-in-flutter
+    String msg = _friends.hasFriend(_currentFriend) ? _friends.historyFor(_currentFriend) : "None";
+    return Expanded(flex: 1, child: SingleChildScrollView(child: Text(msg)));
+  }
+
+  Future<void> send(String msg) async {
+    String response = await _sendToCurrentFriend(msg);
+    setState(() {
+      _sendController.text = response;
+    });
+  }
+
+  Future<String> _sendToCurrentFriend(String msg) async {
+    if (_friends.hasFriend(_currentFriend)) {
+      SocketOutcome sent = await _friends.sendTo(_currentFriend, msg);
+      if (sent.sent) {
+        return "";
+      } else {
+        return sent.errorMessage;
+      }
+    } else {
+      return "Can't send to $_currentFriend";
+    }
   }
 }
