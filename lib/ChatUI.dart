cba1b4fb845de8e16fd2884f0f8cea48e400d0e7
CWTBT <lw03232@gmail.com>
2020-09-30 16:35:52 -0500

    fixed overflow when opening keyboard

--- lib/ChatUI.dart
+++ lib/ChatUI.dart
@@ -18,37 +18,39 @@ class _ChatroomState extends State<Chatroom> {
       body: Container (
         color: Colors.grey[300],
         child: Center (
-          child: Column(
-            children: [
-              Container (
-                decoration: BoxDecoration (
-                  color: Colors.white,
-                  border: Border.all (
-                    color: Colors.black,
+          child: SingleChildScrollView (
+            child: Column(
+              children: [
+                Container (
+                  decoration: BoxDecoration (
+                    color: Colors.white,
+                    border: Border.all (
+                      color: Colors.black,
+                    ),
                   ),
+                  height: 500.0,
+                  width: 400.0,
+                  margin: const EdgeInsets.all(10.0),
                 ),
-                height: 500.0,
-                width: 400.0,
-                margin: const EdgeInsets.all(10.0),
-              ),
-              Container (
-                decoration: BoxDecoration (
-                  color: Colors.white,
-                  border: Border.all (
-                    color: Colors.black,
+                Container (
+                  decoration: BoxDecoration (
+                    color: Colors.white,
+                    border: Border.all (
+                      color: Colors.black,
+                    ),
                   ),
-                ),
-                child: TextField(
-                  decoration: new InputDecoration (
-                    border: InputBorder.none,
-                    focusedBorder: InputBorder.none,
+                  child: TextField(
+                    decoration: new InputDecoration (
+                      border: InputBorder.none,
+                      focusedBorder: InputBorder.none,
 
+                    ),
                   ),
+                  margin: EdgeInsets.all(10.0),
+                  padding: EdgeInsets.fromLTRB(10.0, 0, 0, 0),
                 ),
-                margin: EdgeInsets.all(10.0),
-                padding: EdgeInsets.fromLTRB(10.0, 0, 0, 0),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
